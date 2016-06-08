2320394d19119aae793a66e797ac19110984eab4
Brendan Brown <brendanbrown27@gmail.com>
2016-06-07 21:19:05 -0400

    add routes

--- app/app.js
+++ app/app.js
@@ -2,14 +2,23 @@
 
 angular.module('myApp', ['firebase', 'ngRoute', 'ngLess'])
 	.config(['$routeProvider', function ($routeProvider) {
-		$routeProvider.
-			when('/', {
+		$routeProvider
+			.when('/', {
 				templateUrl: 'partials/home.html'
 			})
 			.when('/music', {
 				templateUrl: 'partials/music.html'
-			}).
-			otherwise({
+			})
+			.when('/music/new', {
+				templateUrl: 'partials/song-new.html'
+			})
+			.when('/music/:id', {
+				templateUrl: 'partials/song.html'
+			})
+			.when('/music/:id/edit', {
+				templateUrl: 'partials/song-edit.html'
+			})
+			.otherwise({
 				redirectTo: '/'
 			});
 	}]);
